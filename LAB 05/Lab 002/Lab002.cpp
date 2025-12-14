1c5ede5d46042c05bf6291395a506ab578d6e683
nattawut mut <6812100005@mut.ac.th>
2025-12-14 17:44:06 +0700

    LAB 05 อัพเดต 14/12/2025

---
+++ LAB 05/Lab 002/Lab002.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string> 
+using namespace std;
+
+int maxOfThree(int a, int b, int c)
+{
+	if (a >= b && a >= c)
+		return a;
+	else if (b >= a && b >= c)
+		return b;
+	else
+		return c;
+}
+
+int main()
+{
+	int a, b, c;
+	int mavxalue = -1000000;
+
+	cout << "Enter a integers: ";
+	cin >> a;
+	cout << "enter b integer: ";
+	cin >> b;
+	cout << "enter c integer: ";
+	cin >> c;
+
+	mavxalue = maxfThree(a, b, c);
+	cout << "the maximum value is: " << mavxalue << endl;
+
+	return 1;
+}
---
+++ LAB 05/Lab003/Lab003.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void swapValues(int& a, int& b)   // pass by reference (C++)
+{
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter a integer: ";
+    cin >> a;
+    cout << "Enter b integer: ";
+    cin >> b;
+
+    cout << "Before swap:" << endl;
+    cout << "a = " << a << " b = " << b << endl;
+
+    swapValues(a, b);
+
+    cout << "After swap:" << endl;
+    cout << "a = " << a << " b = " << b << endl;
+
+    return 0;
+}
